b16dbf7e4506c71852e20ba3ee7618973cd13a5a
Yutong Li <yutongli@google.com>
2020-06-30 18:32:59 +0000

    replace yaml parser to protojson.unmarshal
    Signed-off-by: Yutong Li <yutongli@google.com>

--- csds-client/client_test.go
+++ csds-client/client_test.go
@@ -1,1 +1,7 @@
 package main
+
+import "testing"
+
+func TestParseFlags(t *testing.T){
+
+}
--- csds-client/util.go
+++ csds-client/util.go
@@ -5,9 +5,13 @@ import (
 	"fmt"
 	envoy_type_matcher "github.com/envoyproxy/go-control-plane/envoy/type/matcher"
 	"github.com/ghodss/yaml"
+	"google.golang.org/protobuf/encoding/protojson"
 	"io/ioutil"
 	"path/filepath"
 )
+
+type Nodematchers struct {
+}
 
 func parseYaml(path string, nms *[]*envoy_type_matcher.NodeMatcher) error {
 	filename, _ := filepath.Abs(path)
@@ -19,7 +23,6 @@ func parseYaml(path string, nms *[]*envoy_type_matcher.NodeMatcher) error {
 	if err != nil {
 		return fmt.Errorf("%v", err)
 	}
-	fmt.Println(string(js))
 
 	var data map[string]interface{}
 	err = json.Unmarshal(js, &data)
@@ -28,84 +31,17 @@ func parseYaml(path string, nms *[]*envoy_type_matcher.NodeMatcher) error {
 	}
 
 	for _, n := range data["node_matchers"].([]interface{}) {
-		nMap := n.(map[string]interface{})
 		x := &envoy_type_matcher.NodeMatcher{}
 
-		if metadatas, ok := nMap["node_metadatas"]; ok {
-			var node_metadatas []*envoy_type_matcher.StructMatcher
-			for _, metadata := range metadatas.([]interface{}) {
-				struct_matcher := &envoy_type_matcher.StructMatcher{}
-				mMap := metadata.(map[string]interface{})
-				if paths, ok := mMap["path"]; ok {
-					for _, path := range paths.([]interface{}) {
-						pathMap := path.(map[string]interface{})
-						if key, ok := pathMap["key"]; ok {
-							segment := &envoy_type_matcher.StructMatcher_PathSegment{
-								Segment: &envoy_type_matcher.StructMatcher_PathSegment_Key{Key: key.(string)},
-							}
-							struct_matcher.Path = append(struct_matcher.Path, segment)
-						} else {
-							return fmt.Errorf("missing key in node_metadatas")
-						}
-					}
-				} else {
-					return fmt.Errorf("missing path in node_metadatas")
-				}
-
-				if value, ok := mMap["value"]; ok {
-					valueMap := value.(map[string]interface{})
-					if string_match, ok := valueMap["string_match"]; ok {
-						string_matcher := &envoy_type_matcher.ValueMatcher_StringMatch{}
-						matchMap := string_match.(map[string]interface{})
-						for k, v := range matchMap {
-							switch k {
-							case "exact":
-								string_matcher.StringMatch = &envoy_type_matcher.StringMatcher{MatchPattern: &envoy_type_matcher.StringMatcher_Exact{Exact: v.(string)}}
-							case "prefix":
-								string_matcher.StringMatch = &envoy_type_matcher.StringMatcher{MatchPattern: &envoy_type_matcher.StringMatcher_Prefix{Prefix: v.(string)}}
-							case "suffix":
-								string_matcher.StringMatch = &envoy_type_matcher.StringMatcher{MatchPattern: &envoy_type_matcher.StringMatcher_Suffix{Suffix: v.(string)}}
-							case "regex":
-								string_matcher.StringMatch = &envoy_type_matcher.StringMatcher{MatchPattern: &envoy_type_matcher.StringMatcher_Regex{Regex: v.(string)}}
-							case "safe_regex":
-								string_matcher.StringMatch = &envoy_type_matcher.StringMatcher{MatchPattern: &envoy_type_matcher.StringMatcher_SafeRegex{SafeRegex: &envoy_type_matcher.RegexMatcher{}}} //TODO: fill this field
-							}
-							struct_matcher.Value = &envoy_type_matcher.ValueMatcher{MatchPattern: string_matcher}
-						}
-					}
-				} else {
-					return fmt.Errorf("missing value in node_metadatas")
-				}
-				node_metadatas = append(node_metadatas, struct_matcher)
-			}
-			x.NodeMetadatas = node_metadatas
-		} else {
-			return fmt.Errorf("missing node_metadatas in node_matcher yaml")
+		jsonString, err := json.Marshal(n)
+		if err != nil {
+			return fmt.Errorf("%v", err)
+		}
+		err = protojson.Unmarshal(jsonString, x)
+		if err != nil {
+			return fmt.Errorf("%v", err)
 		}
 
-		if node_id, ok := nMap["node_id"]; ok {
-			node_idMap := node_id.(map[string]interface{})
-
-			if string_match, ok := node_idMap["string_match"]; ok {
-				string_matcher := &envoy_type_matcher.StringMatcher{}
-				matchMap := string_match.(map[string]interface{})
-				for k, v := range matchMap {
-					switch k {
-					case "exact":
-						string_matcher.MatchPattern = &envoy_type_matcher.StringMatcher_Exact{Exact: v.(string)}
-					case "prefix":
-						string_matcher.MatchPattern = &envoy_type_matcher.StringMatcher_Prefix{Prefix: v.(string)}
-					case "suffix":
-						string_matcher.MatchPattern = &envoy_type_matcher.StringMatcher_Suffix{Suffix: v.(string)}
-					case "regex":
-						string_matcher.MatchPattern = &envoy_type_matcher.StringMatcher_Regex{Regex: v.(string)}
-					case "safe_regex":
-						string_matcher.MatchPattern = &envoy_type_matcher.StringMatcher_SafeRegex{&envoy_type_matcher.RegexMatcher{}} //TODO: fill this field
-					}
-				}
-				x.NodeId = string_matcher
-			}
-		}
 		*nms = append(*nms, x)
 	}
 	return nil
