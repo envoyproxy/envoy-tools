73789de5d171d8d0f542ad048fb4cb5fdc3ed255
Yutong Li <yutongli@google.com>
2020-08-14 22:06:29 +0000

    use init() to bind flags
    Signed-off-by: Yutong Li <yutongli@google.com>

--- csds-client/main.go
+++ csds-client/main.go
@@ -8,47 +8,62 @@ import (
 	"time"
 )
 
+// flag vars
+var uri string
+var platform string
+var authnMode string
+var apiVersion string
+var requestFile string
+var requestYaml string
+var jwt string
+var configFile string
+var monitorInterval time.Duration
+var visualization bool
+
+// const default values for flag vars
+const uriDefault string = "trafficdirector.googleapis.com:443"
+const platformDefault string = "gcp"
+const authnModeDefault string = "auto"
+const apiVersionDefault string = "v2"
+const requestFileDefault string = ""
+const requestYamlDefault string = ""
+const jwtDefault string = ""
+const configFileDefault string = ""
+const monitorIntervalDefault time.Duration = 0
+const visualizationDefault bool = false
+
+// init binds flags with valuables
+func init() {
+	flag.StringVar(&uri, "service_uri", uriDefault, "the uri of the service to connect to")
+	flag.StringVar(&platform, "platform", platformDefault, "the platform (e.g. gcp, aws,  ...)")
+	flag.StringVar(&authnMode, "authn_mode", authnModeDefault, "the method to use for authentication (e.g. auto, jwt, ...)")
+	flag.StringVar(&apiVersion, "api_version", apiVersionDefault, "which xds api major version to use (e.g. v2, v3 ...)")
+	flag.StringVar(&requestFile, "request_file", requestFileDefault, "yaml file that defines the csds request")
+	flag.StringVar(&requestYaml, "request_yaml", requestYamlDefault, "yaml string that defines the csds request")
+	flag.StringVar(&jwt, "jwt_file", jwtDefault, "path of the -jwt_file")
+	flag.StringVar(&configFile, "output_file", configFileDefault, "file name to save configs returned by csds response")
+	flag.DurationVar(&monitorInterval, "monitor_interval", monitorIntervalDefault, "the interval of sending request in monitor mode (e.g. 500ms, 2s, 1m ...)")
+	flag.BoolVar(&visualization, "visualization", visualizationDefault, "option to visualize the relationship between xDS")
+}
+
 func main() {
-	// parse flags
-	const uriDefault string = "trafficdirector.googleapis.com:443"
-	const platformDefault string = "gcp"
-	const authnModeDefault string = "auto"
-	const apiVersionDefault string = "v2"
-	const requestFileDefault string = ""
-	const requestYamlDefault string = ""
-	const jwtDefault string = ""
-	const configFileDefault string = ""
-	const monitorIntervalDefault time.Duration = 0
-	const visualizationDefault bool = false
-
-	uriPtr := flag.String("service_uri", uriDefault, "the uri of the service to connect to")
-	platformPtr := flag.String("platform", platformDefault, "the platform (e.g. gcp, aws,  ...)")
-	authnModePtr := flag.String("authn_mode", authnModeDefault, "the method to use for authentication (e.g. auto, jwt, ...)")
-	apiVersionPtr := flag.String("api_version", apiVersionDefault, "which xds api major version to use (e.g. v2, v3 ...)")
-	requestFilePtr := flag.String("request_file", requestFileDefault, "yaml file that defines the csds request")
-	requestYamlPtr := flag.String("request_yaml", requestYamlDefault, "yaml string that defines the csds request")
-	jwtPtr := flag.String("jwt_file", jwtDefault, "path of the -jwt_file")
-	configFilePtr := flag.String("output_file", configFileDefault, "file name to save configs returned by csds response")
-	monitorIntervalPtr := flag.Duration("monitor_interval", monitorIntervalDefault, "the interval of sending request in monitor mode (e.g. 500ms, 2s, 1m ...)")
-	visualizationPtr := flag.Bool("visualization", visualizationDefault, "option to visualize the relationship between xDS")
-
 	flag.Parse()
 
 	clientOpts := client.ClientOptions{
-		Uri:             *uriPtr,
-		Platform:        *platformPtr,
-		AuthnMode:       *authnModePtr,
-		RequestFile:     *requestFilePtr,
-		RequestYaml:     *requestYamlPtr,
-		Jwt:             *jwtPtr,
-		ConfigFile:      *configFilePtr,
-		MonitorInterval: *monitorIntervalPtr,
-		Visualization:   *visualizationPtr,
+		Uri:             uri,
+		Platform:        platform,
+		AuthnMode:       authnMode,
+		RequestFile:     requestFile,
+		RequestYaml:     requestYaml,
+		Jwt:             jwt,
+		ConfigFile:      configFile,
+		MonitorInterval: monitorInterval,
+		Visualization:   visualization,
 	}
 
 	var c client.Client
 	var err error
-	if *apiVersionPtr == "v2" {
+	if apiVersion == "v2" {
 		c, err = client_v2.New(clientOpts)
 	} else {
 		log.Fatal("invalid api version")
